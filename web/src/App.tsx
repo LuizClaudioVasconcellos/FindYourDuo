464d6cecacd4f54e0c7c8b7d73b18685a07ca794
LuizClaudioVasconcellos <luizvasconcellosjunior@gmail.com>
2022-11-22 19:28:50 -0300

    create web components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
-import { MagnifyingGlassPlus } from "phosphor-react";
+import { GameBanner } from "./components/Gamerbanner";
+import { CreateAdBanner } from "./components/CreateAdBanner";
 
 import "./styles/main.css";
 
@@ -20,77 +21,14 @@ function App() {
         </h1>
       </div>
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-1.png" alt="" />
+        <GameBanner
+          bannerUrl="/game-1.png"
+          title="League of Legends"
+          adsCount={5}
+        />
+        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={3} />
 
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              League of Legends
-            </strong>
-            <span className="text-sm text-zinc-300 block">4 anúcios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-2.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Dota 2</strong>
-            <span className="text-sm text-zinc-300 block">4 anúcios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-3.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              Counter Strike
-            </strong>
-            <span className="text-sm text-zinc-300 block">4 anúcios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-4.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Apex Legends</strong>
-            <span className="text-sm text-zinc-300 block">4 anúcios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-5.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Fortnite</strong>
-            <span className="text-sm text-zinc-300 block">4 anúcios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-6.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              World of Worcraft
-            </strong>
-            <span className="text-sm text-zinc-300 block">4 anúcios</span>
-          </div>
-        </a>
-        <div className="pt-1 bg-nlw-gradient col-span-6 rounded-lg mt-8 overflow-hidden">
-          <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-            <div>
-              <strong className="text-2xl text-white font-black block">
-                Não encontrou seu duo?
-              </strong>
-              <span className="text-zinc-400 block">
-                Publique um anúncio para encontrar novos players!
-              </span>
-            </div>
-
-            <button className="px-3 py-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-              <MagnifyingGlassPlus size={24} />
-              &#160;Publicar anúncio
-            </button>
-          </div>
-        </div>
+        <CreateAdBanner />
       </div>
     </div>
   );
